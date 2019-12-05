7b7f276582a93cd207a22e478bc5bf7bda2076f7
BuildTools <unconfigured@null.spigotmc.org>
2019-12-06 00:53:30 +0300

    config updated

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 
 CFG = {
     'mqtt': {
-        'host': 'iotfox.ru',
-        'port': 20002
+        'host': '',
+        'port': 
     },
     'db': {
-        'host': 'iotfox.ru',
-        'port': 20004,
-        'user': 'postgres',
-        'password': '1234',
-        'dbname': 'IoTSystemDB',
-        'table_last_values': 'device.external_mqtt_broker_last_values',
-        'table_history': 'device.external_mqtt_broker_history'
+        'host': '',
+        'port': 0,
+        'user': '',
+        'password': '',
+        'dbname': '',
+        'table_last_values': '',
+        'table_history': ''
     }
 }
